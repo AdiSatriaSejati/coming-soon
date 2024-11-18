9fb8c9eb9c0b348190d7199fd7c0a44348dd54ad
AdiSatria15_ <12221455@bsi.ac.id>
2024-11-18 14:04:53 +0700

    remove loading

--- src/App.jsx
+++ src/App.jsx
@@ -8,42 +8,30 @@ import { Gallery } from "./components/gallery";
 // import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
-import Loading from "./components/Loading";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
-import "./styles/loading.css"
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-
+  const [landingPageData, setLandingPageData] = useState({});
   useEffect(() => {
-    const loadingDuration = 7400;
-
-    const timer = setTimeout(() => {
-      setLandingPageData(JsonData);
-      setIsLoaded(false);
-    }, loadingDuration);
-
-    return () => clearTimeout(timer);
+    setLandingPageData(JsonData);
   }, []);
 
   return (
-    <div className={`app-container ${isLoaded ? 'fade-in' : 'fade-out'}`}>
-      {!landingPageData ? (
-        <Loading />
-      ) : (
-        <>
-          <Navigation />
-          <Header data={landingPageData.Header} />
-          <About data={landingPageData.About} />
-          <Services data={landingPageData.Services} />
-          <Gallery data={landingPageData.Gallery} />
-          <Team data={landingPageData.Team} />
-          <Contact data={landingPageData.Contact} />
-        </>
-      )}
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
     </div>
   );
 };
